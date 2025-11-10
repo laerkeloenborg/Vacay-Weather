325d52b4f328a5085fc6e04fed0a5da53c43b1f7
KasperPersson <kape0010@stud.kea.dk>
2025-11-10 11:45:42 +0100

    DTOS + webconfig + API-key env var

---
+++ src/main/java/org/example/findmygate/dto/Choice.java
@@ -0,0 +1,72 @@
+
+package org.example.findmygate.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "index",
+    "finish_reason",
+    "message"
+})
+@Generated("jsonschema2pojo")
+public class Choice {
+
+    @JsonProperty("index")
+    private Integer index;
+    @JsonProperty("finish_reason")
+    private String finishReason;
+    @JsonProperty("message")
+    private Message message;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("index")
+    public Integer getIndex() {
+        return index;
+    }
+
+    @JsonProperty("index")
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    @JsonProperty("finish_reason")
+    public String getFinishReason() {
+        return finishReason;
+    }
+
+    @JsonProperty("finish_reason")
+    public void setFinishReason(String finishReason) {
+        this.finishReason = finishReason;
+    }
+
+    @JsonProperty("message")
+    public Message getMessage() {
+        return message;
+    }
+
+    @JsonProperty("message")
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
