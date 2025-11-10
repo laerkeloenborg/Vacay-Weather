ee007d244f5fcc30939985a7d3203d16800a940f
laerkeloenborg <lalo0001@stud.kea.dk>
2025-11-10 10:17:28 +0100

    setup

---
+++ src/main/java/org/example/findmygate/FindMyGateApplication.java
@@ -0,0 +1,13 @@
+package org.example.findmygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/findmygate/FindMyGateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.findmygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
