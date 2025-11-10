8c9ef9d9c2f58d610bfbbba786d2fdf86d83d829
KasperPersson <kape0010@stud.kea.dk>
2025-11-10 12:08:26 +0100

    Initital setup with controller, service, config, dtos and env vars

--- src/main/java/org/example/findmygate/controller/MistralController.java
+++ src/main/java/org/example/findmygate/controller/MistralController.java
@@ -1,12 +1,22 @@
 package org.example.findmygate.controller;
 
+import org.example.findmygate.service.MistralService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MistralController {
 
-    @Value("${open.mistral.api.key}")
-    private String openapikey;
+    @Autowired
+    private MistralService mistralService;
 
+    @GetMapping("/test")
+    public Map<String, Object> test() {
+        Map<String, Object> testmap = mistralService.promptMistral();
+        return testmap;
+    }
 }
